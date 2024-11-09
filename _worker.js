3dfaf950344c9ba7149eccd6f1148229ff4c803c
carlcolin <164457422+carlcolin@users.noreply.github.com>
2024-11-09 11:35:32 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4328,7 +4328,7 @@ var worker_default = {
               }
             });
           default:
-            url.hostname = "alist.dokii.tk";
+            url.hostname = "alist.skye.tk";
             url.protocol = "https:";
             request = new Request(url, request);
             return await fetch(request);
