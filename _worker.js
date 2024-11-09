73e82fc777d061e2199bf05cd658489578661beb
carlcolin <164457422+carlcolin@users.noreply.github.com>
2024-11-09 11:32:01 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4328,7 +4328,7 @@ var worker_default = {
               }
             });
           default:
-            url.hostname = "www.speedtest.net";
+            url.hostname = "alist.dokii.tk";
             url.protocol = "https:";
             request = new Request(url, request);
             return await fetch(request);
